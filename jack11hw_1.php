d91aa8b248161be91fed2fbbb95d3e762a870464
chenjiahui <chancemy80468@gmail.com>
2018-04-27 01:14:20 +0800

    180427_1

--- jack11hw_1.php
+++ jack11hw_1.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chenjiahui
- * Date: 2018/4/27
- * Time: 上午12:45
- */
+    $x = '';
+    $y = '';
+    $resultQuo = '';
+    $resultRem = '';
+    $ans = '';
+    if(isset($_GET['x'])){
+        $x=$_GET['x'];
+        $y=$_GET['y'];
+        $resultQuo=(int)($x/$y);
+        $resultRem=$x%$y;
+        $ans = $resultQuo ."......".$resultRem;
+    }
+?>
+<form>
+    <input type="text" name="x" value="<?php echo $x ?>" />
+    %
+    <input type="text" name="y" value="<?php echo $y ?>" />
+    <input type="submit" name="result" value="="/>
+    <?php echo $ans; ?>
+</form>
